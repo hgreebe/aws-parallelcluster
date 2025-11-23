59a7aefff92c596471bc5860f95c8364b06a7f5f
Helena Greebe <hgreebe@amazon.com>
2025-11-23 14:59:46 -0500

    unset library path

--- cli/src/pcluster/api/util.py
+++ cli/src/pcluster/api/util.py
@@ -185,16 +185,16 @@ def _assert_node_version():
         #   PATH
         # [B603:subprocess_without_shell_equals_true] Is suppressed because input of check_output is not coming from
         #   untrusted source
-        # env_output = subprocess.check_output(
-        #     ["env"],
-        #     stderr=subprocess.STDOUT,
-        #     shell=False,
-        #     encoding="utf-8"
-        # )
-        env_output = "test"
+        env_output = subprocess.check_output(
+            ["env"],
+            stderr=subprocess.STDOUT,
+            shell=False,
+            encoding="utf-8"
+        )
+        # env_output = "test"
         LOGGER.critical("Environment from 'env' command:%s", env_output)
         node_version_string = subprocess.check_output(  # nosec B607 B603
-            ["node", "--version"], stderr=subprocess.STDOUT, shell=False, encoding="utf-8"
+            'unset LD_LIBRARY_PATH && node --version', stderr=subprocess.STDOUT, shell=False, encoding="utf-8"
         )
         LOGGER.debug("Found Node.js version (%s)", node_version_string)
     except subprocess.CalledProcessError as e:
